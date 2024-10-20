b2c8d79576ab8cce8679a34d8bd7da488334b1fe
Zoyaalihullio <zoyaalihullio@gmail.com>
2024-10-20 19:36:01 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,12 +2,12 @@ const fs = require('fs');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
-}
+
 
 module.exports = {
-    
-SESSION_ID: process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID,
-OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '' : process.env.OWNER_NUMBER,   
+    ll
+SESSION_ID: process.env.SESSION_ID === undefined ? VAJIRA-MD=LAMUXb6b#lSqep1E4abJG8Ty6ZQUjZrAh_S7JyY3d6z99Oaz349o : process.env.SESSION_ID,
+OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? VAJIRA-MD=LAMUXb6b#lSqep1E4abJG8Ty6ZQUjZrAh_S7JyY3d6z99Oaz349o' : process.env.OWNER_NUMBER,   
 ONLY_GROUP: process.env.ONLY_GROUP === undefined ? 'false' : process.env.ONLY_GROUP,
 ONLY_ME: process.env.ONLY_ME === undefined ? 'false' : process.env.ONLY_ME,
 AUTO_STATUS_READ:  process.env.AUTO_STATUS_READ  || false  ,    
